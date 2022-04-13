af30dd126330d390416d7ca18a06e587f77ea03c
Alexander San Miguel <alexandersanmig@gmail.com>
2022-04-13 18:14:09 +0300

    Got started on creating a database connector for the project. Still very experimental. - Alex

---
+++ TickTimer/DAO/AsyncLazy.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TickTimer.DAO
+{
+    class AsyncLazy<T>
+    {
+        readonly Lazy<Task<T>> instance;
+
+        public AsyncLazy(Func<T> factory)
+        {
+            instance = new Lazy<Task<T>>(() => Task.Run(factory));
+        }
+
+        public AsyncLazy(Func<Task<T>> factory)
+        {
+            instance = new Lazy<Task<T>>(() => Task.Run(factory));
+        }
+
+        public TaskAwaiter<T> GetAwaiter()
+        {
+            return instance.Value.GetAwaiter();
+        }
+    }
+}
---
+++ TickTimer/DAO/LocalDAO.cs
@@ -0,0 +1,27 @@
+﻿using SQLite;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TickTimer.DAO
+{
+    class LocalDAO
+    {
+        static SQLiteAsyncConnection Database;
+
+        public static readonly AsyncLazy<LocalDAO> Instance = new AsyncLazy<LocalDAO>(async () =>
+        {
+            var instance = new LocalDAO();
+            CreateTableResult result = await Database.CreateTableAsync<ProcessTimer>()
+            return instance;
+        });
+
+        public LocalDAO()
+        {
+            Database = new SQLiteAsyncConnection(Constants.DatabasePath, Constants.Flags);
+        }
+
+    }
+}
